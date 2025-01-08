55130b82c617062d7a86c1429e26e5bf2f702c19
KeyurD1010 <keyurdhanani456@gmail.com>
2025-01-08 19:16:28 +0530

    Project Done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Generative AI Projec',
+    version="0.0.0",
+    author="Keyur Dhanani",
+    author_email="keyurdhanani456@gmai.com",
+    packages=find_packages(),
+    install_requirement =[]
+)
